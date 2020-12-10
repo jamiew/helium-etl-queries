c02e56621c755474efa89141ff57a64e68ed5219
Jamie Wilkinson <jamie@jamiedubs.com>
2020-12-09 21:43:41 -0500

    Draft view for oracle_price_transactions

---
+++ views/oracle_price_transactions.sql
@@ -0,0 +1,18 @@
+CREATE VIEW oracle_price_transactions AS
+SELECT
+    public.transactions.block AS block,
+    public.transactions.hash AS hash,
+    public.transactions.type AS type,
+
+    -- fields specific to this txn
+    public.transactions.fields->>'fee' as fee,
+    public.transactions.fields->>'price' as price, -- do we want to cast this?
+    public.transactions.fields->>'public_key' as public_key,
+    public.transactions.fields->>'block_height' as block_height, -- is this
+
+    to_timestamp(public.transactions.time) AS time
+FROM
+  public.transactions
+WHERE
+  public.transactions.type = CAST('poc_request_v1' AS transaction_type)
+;
