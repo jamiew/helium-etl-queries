79b5aaace9567dda07810752990cfc9d6f510b70
Jamie Wilkinson <jamie@jamiedubs.com>
2021-02-22 09:43:31 -0500

    Bump lowest block limit for challenge_receipts_parsed view

--- views/challenge_receipts_parsed.sql
+++ views/challenge_receipts_parsed.sql
@@ -6,7 +6,7 @@ with data1 as
 SELECT  a.block, a.hash, a.time, b.value as cpath
 FROM
   public.challenge_receipts a, json_array_elements(a.path::json) b
-  where a.block > 635109
+  where a.block > 675109
 ),
 data2 as (
 select  a.block, a.hash, a.time,
