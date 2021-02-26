7055b354c37bbe002e5db9834b4a4af9d2785ddf
Jamie Wilkinson <jamie@jamiedubs.com>
2021-02-26 12:38:11 -0500

    Re-add indexes. Whoops!

--- migrations/0001_replace_challenge_receipts_parsed.sql
+++ migrations/0001_replace_challenge_receipts_parsed.sql
@@ -6,4 +6,12 @@ ALTER MATERIALIZED VIEW challenge_receipts_parsed RENAME TO mv_challenge_receipt
 CREATE TABLE challenge_receipts_parsed AS
   SELECT * FROM mv_challenge_receipts_parsed;
 
+-- blow away old indexes and make some fresh ones
+DROP INDEX IF EXISTS challenge_receipts_parsed_transmitter_address_idx;
+CREATE INDEX challenge_receipts_parsed_transmitter_address_idx ON public.challenge_receipts_parsed USING btree (transmitter_address);
+DROP INDEX IF EXISTS challenge_receipts_parsed_transmitter_name_idx;
+CREATE INDEX challenge_receipts_parsed_transmitter_name_idx ON public.challenge_receipts_parsed USING btree (transmitter_name);
+DROP INDEX IF EXISTS challenge_receipts_parsed_witness_name_idx;
+CREATE INDEX challenge_receipts_parsed_witness_name_idx ON public.challenge_receipts_parsed USING btree (witness_name);
+
 DROP MATERIALIZED VIEW mv_challenge_receipts_parsed;
