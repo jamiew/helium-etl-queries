df71de9b9d8ee8aa2658f3aca49b0afcf50247a6
Greg Karabinos <gkarabinos@Gregs-MacBook-Pro.local>
2021-04-19 22:01:12 -0400

    added additional witness data to view

--- views/challenge_receipts_parsed.sql
+++ views/challenge_receipts_parsed.sql
@@ -28,7 +28,10 @@ SELECT  a.block, a.hash, a.challengee,
         b.value::json->>'is_valid' as witness_is_valid,
         b.value::json->>'invalid_reason' as witness_invalid_reason,
         b.value::json->>'signal' as witness_signal,
-        b.value::json->>'snr' as witness_snr
+        b.value::json->>'snr' as witness_snr,
+        b.value::json->>'channel' as witness_channel,
+        b.value::json->>'datarate' as witness_sf,
+        b.value::json->>'frequency' as witness_frequency
 from    data2 a, json_array_elements(a.witnesses::json) b
 ),
 hotspot1 as (
@@ -38,7 +41,7 @@ from    gateway_inventory
 Insert into challenge_receipts_parsed
 select  a.block, a.hash, a.time, h.name as transmitter_name, a.challengee_gateway as transmitter_address, a.origin,
         b.witness_owner, wt.name as witness_name, b.witness_gateway, COALESCE(b.witness_is_valid, 'No Witness') as witness_is_valid,
-        b.witness_invalid_reason, b.witness_signal, b.witness_snr
+        b.witness_invalid_reason, b.witness_signal, b.witness_snr, b.witness_channel, b.witness_sf, b.witness_frequency
 from    data_r1 a
 join    hotspot1 h
     on  a.challengee_gateway = h.address
