6f66477619c00de9fc922ef4aa6018cbe4e3e6fd
wishplorer <74160072+wishplorer@users.noreply.github.com>
2021-07-11 11:18:07 -0700

    update validator penalty view
    added owner column

--- views/validator_penalty_parsed.sql
+++ views/validator_penalty_parsed.sql
@@ -1,13 +1,13 @@
-create or replace view validator_penalty_parsed as 
+create view validator_penalty_parsed as 
 	with data1 as (
-		select 	address, name,
+		select 	address, name, owner,
 			b.value as cpen
 		from 	public.validator_inventory a, json_array_elements(a.penalties::json) as b
 	), data2 as (
-		select  address, name,
-				cpen::json->>'type' as penalty_type,
-				(cpen::json->>'amount')::double precision as penalty_amount,
-				(cpen::json->>'height')::bigint as penalty_height
+		select  address, name, owner,
+			cpen::json->>'type' as penalty_type,
+			(cpen::json->>'amount')::double precision as penalty_amount,
+			(cpen::json->>'height')::bigint as penalty_height
 		from	data1
 	)
 	select 	* 
