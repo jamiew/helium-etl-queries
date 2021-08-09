f99f63066622bce8069c75fdd4302fa5cc68fdec
Jamie Dubs <jamie@jamiedubs.com>
2021-08-09 10:40:18 -0400

    Add payment_transactions view (#15)
    * Add payment_transactions view
    
    * Rename

---
+++ views/transactions_payment.sql
@@ -0,0 +1,16 @@
+drop view if exists transactions_payment;
+create view transactions_payment as
+	select 
+			block
+			-- , hash
+			-- , type
+			, fields::json->>'fee' as fee
+			, fields::json->>'hash' as hash
+			, fields::json->>'type' as type
+			, fields::json->>'nonce' as nonce
+			, fields::json->>'payee' as payee
+			, fields::json->>'payer' as payer
+			, fields::json->>'amount' as amount    
+	from transactions 
+	where type IN ('payment_v1', 'payment_v2');
+
