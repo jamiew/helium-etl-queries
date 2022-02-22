818dee52707b4b46d3c1249a30e7aba708820b10
Jamie Wilkinson <jamie@jamiedubs.com>
2022-02-22 17:49:44 -0500

    Set query timeout to 0 for long-running cron queries

--- crontab-daily.sql
+++ crontab-daily.sql
@@ -1,2 +1,3 @@
 -- meant to be run nightly; quite slow
+set statement_timeout to 0;
 REFRESH MATERIALIZED VIEW data_credits;
--- crontab-hourly.sql
+++ crontab-hourly.sql
@@ -1,2 +1,3 @@
 -- meant to be run frequently. could be more often than an hour, but hourly is fine
+set statement_timeout to 0;
 \i ./views/challenge_receipts_parsed.sql;
